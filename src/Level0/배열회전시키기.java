497695297e3c99c615a64e4955ea0ae889470722
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-12-19 18:41:11 +0900

    221219

--- src/Level0/배열회전시키기.java
+++ src/Level0/배열회전시키기.java
@@ -1,11 +1,41 @@
 package Level0;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class 배열회전시키기 {
 
 	public static void main(String[] args) {
 		// 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 배열 numbers의 원소를 direction방향으로
 		// 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
+		int[] numbers = {4, 455, 6, 4, -1, 45, 6};
+		String direction = "left";
+		int[] answer = new int[numbers.length];
+		int length = numbers.length;
+		for(int i = 0; i < length ; i++) {
+			switch(direction) {
+			case "left":
+				answer[i] = i == length-1 ? numbers[0] : numbers[i+1];
+				break;
+			case "right":
+				answer[i] = i == 0 ? numbers[length-1] : numbers[i-1];
+				break;
+			}
+			System.out.print(answer[i] +",");
+		}
+		
+		//stream 사용 
+		List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
 
+        if (direction.equals("right")) {
+            list.add(0, list.get(list.size() - 1));
+            list.remove(list.size() - 1);
+        } else {
+            list.add(list.size(), list.get(0));
+            list.remove(0);
+        }
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
 	}
 
 }
---
+++ src/Level0/최빈값구하기.java
@@ -0,0 +1,44 @@
+package Level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 최빈값구하기 {
+
+	public static void main(String[] args) {
+		// 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록
+		// solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+		int[] array = {1,1};
+		int answer = 0;
+		int count = 0;
+		int controll = -1;
+		int last = 0;
+		for(int i = 0; i<array.length ; i++) {
+			count = 0;
+			for(int j = 0; j<array.length; j++) {
+				if(controll == array[i]) {
+					break;
+				}
+				if(array[i] == array[j]) {
+					count++;
+				}
+			}
+			if(controll != array[i]) {
+				if(count > answer) {
+					answer = count;
+					controll = array[i];
+					last = 0;
+				}else if(count == answer) {
+					last = 1;
+				}
+			}
+			
+		}
+		if(last == 1) {
+			answer = -1;
+		}
+		System.out.println(answer);
+
+	}
+
+}
