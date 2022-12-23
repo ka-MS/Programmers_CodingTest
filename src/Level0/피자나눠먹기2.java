7374b1dc15b5b26c42db926d57493ccfad7546f0
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-12-23 23:55:40 +0900

    221223

--- src/Level0/피자나눠먹기2.java
+++ src/Level0/피자나눠먹기2.java
@@ -5,9 +5,8 @@ public class 피자나눠먹기2 {
 	public static void main(String[] args) {
 		// 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, n명이 주문한 피자를 남기지
 		// 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
-		int n = 7;
-		int answer = 0;
-//		int i = 1;
+		int n = 10;
+		int answer = 1;
 		for(int i = 1; i <=6 ; i++) {
 			if((n*i)%6 == 0) {
 				answer = n*i/6;
