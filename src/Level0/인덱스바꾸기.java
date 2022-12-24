1ee725b9de1555d572861971181fec443c44ea8d
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-12-24 23:58:13 +0900

    221224

---
+++ src/Level0/인덱스바꾸기.java
@@ -0,0 +1,44 @@
+package Level0;
+
+public class 인덱스바꾸기 {
+
+	public static void main(String[] args) {
+		// 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에
+		// 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+		String my_string = "hello";
+		int num1 = 1;
+		int num2 = 2;
+		String answer = "";
+
+		char[] arr = { my_string.charAt(num1), my_string.charAt(num2) };
+		String[] str = my_string.split("");
+		for (int i = 0; i < str.length; i++) {
+			if (i == num1) {
+				answer += arr[1];
+			} else if (i == num2) {
+				answer += arr[0];
+			} else {
+				answer += str[i];
+			}
+		}
+		answer = String.valueOf(arr);
+		System.out.println(answer);
+
+		// char배열 + String.valueOf 풀이
+		char[] ch = my_string.toCharArray();
+
+		ch[num1] = my_string.charAt(num2);
+		ch[num2] = my_string.charAt(num1);
+
+		answer = String.valueOf(ch);
+		// 얕은복사 깊은복사 풀이
+		String[] arr2 = my_string.split("");
+        String temp = arr2[num1];
+        arr[num1] = arr[num2];
+//        arr[num2] = temp;
+        for(String str2 : arr2){
+            answer += str;
+        }
+	}
+
+}
--- src/Level0/최댓값만들기2.java
+++ src/Level0/최댓값만들기2.java
@@ -5,25 +5,21 @@ public class 최댓값만들기2 {
 	public static void main(String[] args) {
 		// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록
 		// solution 함수를 완성해주세요.
-		
-		int[] numbers = {-500 , 2};
-		int answer = 0;
-		
-		for(int i = 0; i<numbers.length ; i++) {
-			for(int k = 0; k<numbers.length;k++ ) {
-				if(i != k) {
-					if(numbers[i]*numbers[k] > answer) {
-						answer = numbers[i]*numbers[k];
+
+		int[] numbers = { -500, 2 };
+		int answer = numbers[0] * numbers[1];
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int k = 0; k < numbers.length; k++) {
+				if (i != k) {
+					if (numbers[i] * numbers[k] > answer) {
+						answer = numbers[i] * numbers[k];
 					}
-					
 				}
 			}
 		}
-		if(numbers.length == 2) {
-			answer = numbers[0]*numbers[1];
-		}
 		System.out.println(answer);
-			
+
 	}
 
 }
